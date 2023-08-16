dc9e809e12e38ec048bc6250891955cbcfd7d9eb
kerem <kerem_bolt@hotmail.com>
2023-08-16 17:20:56 +0300

    mongoprob

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 
-const Product = require('../models/products')
+const Product = require('../models/product')
 
 router.get('/', (req, res, next) => {
     res.status(200).json({
@@ -10,10 +11,17 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
-    const product = {
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
         price: req.body.price
-    }
+    })
+    product
+    .save()
+    .then(result => {
+        console.log(result)
+    })
+    .catch(err => console.log(err))
     res.status(201).json({
         message: 'Handling Post request to /products',
         createdProduct: product
--- app.js
+++ app.js
@@ -6,28 +6,31 @@ const mongoose = require('mongoose')
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
-
+// mongodb+srv://thealpha:<password>@atlascluster.7jgaehh.mongodb.net/?retryWrites=true&w=majority
 mongoose.connect(
-"mongodb+srv://thealpha:"
-+ process.env.MONGO_ATLAS_PW +
-" @atlascluster.7jgaehh.mongodb.net/?retryWrites=true&w=majority",
-{
-    useMongoClient: true}
-)
+"mongodb+srv://user2000:"
++process.env.MONGO_ATLAS_PW+
+"@atlascluster.7jgaehh.mongodb.net/?retryWrites=true&w=majority",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }
+).catch(error => console.error("MongoDB connection error:", error));
 
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
 app.use((req, res , next) => {
-    res.header('Acces-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header(
-    'Acces-Control-Allow-Headers',
+    'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept, Authorization '); 
 if (req.method === 'OPTIONS') {
     res.header('Acces-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
     return res.status(200).json({});    
 }
+next()
 });
 
 // Routes which should handle requests
